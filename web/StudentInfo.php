fa9cb6b55d312709b81550f4105116ce4bf96f4f
Daniel_Thomsen <tho13020@byui.edu>
2018-10-02 16:44:26 -0600

    some php

--- web/StudentInfo.php
+++ web/StudentInfo.php
@@ -11,11 +11,20 @@
     <a href="mailto:<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a><br>
 <?php echo $_POST["major"]; ?><br>
 <?php echo $_POST["comments"]; ?><br>
-<?php $continents = $_POST["Continent"];
-    
-    foreach( $continents as $key){
+<?php 
+//    $continents = $_POST["Continent"];
+    if (isset($_POST['Continent']))
+    {
+        $continents = $_POST['Continent']
+        foreach( $continents as $key){
         echo "$key <br>";
     }
+    }
+    
+    
+//    foreach( $continents as $key){
+//        echo "$key <br>";
+//    }
     
 ?>
 </body>
