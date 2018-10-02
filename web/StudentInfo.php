d958833884c42429ee4ef7a740b3eadb9ba6e43f
Daniel_Thomsen <tho13020@byui.edu>
2018-10-02 16:16:30 -0600

    student info pages

---
+++ web/StudentInfo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<?php echo $_POST["name"]; ?><br>
+<?php echo $_POST["email"]; ?><br>
+<?php echo $_POST["major"]; ?><br>
+<?php echo $_POST["comments"]; ?><br>
+
+
+</body>
+
+</html>
