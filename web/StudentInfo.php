6e3d1741d322c3cf7537ac412c0f065485492193
Daniel_Thomsen <tho13020@byui.edu>
2018-10-02 16:53:33 -0600

    some php

--- web/StudentInfo.php
+++ web/StudentInfo.php
@@ -17,11 +17,14 @@
 //    $continents = $_POST["Continent"];
 //    if (isset($_POST['Continent']))
 //    {
-        if (!empty($_POST[Continent])){
+        if (!empty($_POST['Continent'])){
+            
         $continents = $_POST['Continent']
+            
         foreach( $continents as $key){
-        echo "$key <br>";
-    }
+        
+            echo "$key <br>";
+        }
     }
     
     
