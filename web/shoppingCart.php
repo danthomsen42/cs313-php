5afaa66fa1c63a341e089dee7a82b8c66655ca57
Daniel_Thomsen <tho13020@byui.edu>
2018-10-06 23:04:08 -0600

    some php

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -13,14 +13,7 @@ session_start();
 
 <body>
 
-<!--
-    <?php echo $_POST["name"]; ?><br>
-    <a href="mailto:<?php echo $_POST[" email "]; ?>">
-        <?php echo $_POST["email"]; ?>
-    </a><br>
-    <?php echo $_POST["major"]; ?><br>
-    <?php echo $_POST["comments"]; ?><br>
--->
+
     <?php 
 if(!empty($_POST['Merchandise'])){
     foreach($_POST['Merchandise'] as $value){
