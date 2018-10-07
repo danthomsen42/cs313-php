708726972aa6615f33d7867d7d4abd288469653f
Daniel_Thomsen <tho13020@byui.edu>
2018-10-06 23:14:49 -0600

    some php

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -20,6 +20,14 @@ if(!empty($_POST['Merchandise'])){
         echo $value.'<br>';
     }
 }
+    
+    
+    $value1 = $_POST['number1'];
+    $value2 = $_POST['number2'];
+    $value3 = $_POST['number3'];
+    $value4 = $_POST['number4'];
+    
+    
     $_SESSION['number1'] = $value1;
     $_SESSION['number2'] = $value2;
     $_SESSION['number3'] = $value3;
