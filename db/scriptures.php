095a3835a747b0e9d3a8065ca8626425ee988d86
Daniel_Thomsen <tho13020@byui.edu>
2018-10-16 16:17:05 -0600

    group project

--- db/scriptures.php
+++ db/scriptures.php
@@ -1,23 +1,25 @@
 <html>
+
 <head>
-    
-    
-    
-    </head>
+
+
+
+</head>
+
 <body>
     <?php require 'resources.php'?>
-    
+
     <?php 
     foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
 {
-  echo '<b>' . $row['book']. '&nbsp' . $row['chapter'] . ':' . $row['verse'] . '</b> - \"' . $row['content'] . '\"';
+  echo '<b>' . $row['book'] . '&nbsp' . $row['chapter'] . ':' . $row['verse'] . '</b> - \"' . $row['content'] . '\"';
 //  echo ' password: ' . $row['password'];
   echo '<br/>';
 }
     ?>
-    
-    </body>
+
+</body>
 
 
 
-</html>    
+</html>
