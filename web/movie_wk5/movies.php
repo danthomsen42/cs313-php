3a7e241b9d5484c029b65e93e2d59148c8a6f7c4
Daniel_Thomsen <tho13020@byui.edu>
2018-10-18 16:48:41 -0600

    movie db practice

--- web/movie_wk5/movies.php
+++ web/movie_wk5/movies.php
@@ -22,7 +22,7 @@ $rows = $stmt->fetchAll(PDO::FETHC_ASSOC);
        foreach ($movies as $movie) {
            $title = $movie['title'];
            $year = $movie['year'];
-           echo "<li><p>$title ($year)</p></li>"
+           echo "<li><p>$title ($year)</p></li>";
        }   
           ?>
     </ul>
