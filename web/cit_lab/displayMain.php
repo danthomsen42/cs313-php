3707460c4f845936f1cc43f4c4b2a93fb04e38bc
Daniel_Thomsen <tho13020@byui.edu>
2018-10-20 19:59:19 -0600

    mah first attempt at sending to mah db

--- web/cit_lab/displayMain.php
+++ web/cit_lab/displayMain.php
@@ -4,9 +4,12 @@ require('dbConnect.php');
 //$db = get_db();
 //query for all movies
 $stmt = $db->prepare('SELECT id, ast_name FROM assistants');
+$meh = $db->prepare('SELECT id, courseCode FROM classes');
 $stmt-> execute();
+$meh-> execute();
 
 $ast = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$cls = $meh->fetchAll(PDO::FETCH_ASSOC);
 // go through each movie in th eresult and display it
 
 ?>
@@ -19,10 +22,17 @@ $ast = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <ul>
     <?php
+       foreach ($cls as $cls) {
+           $classcode = $cls['course_Code'];
+//           $course_code = $ast['courseCode'];
+           echo "<li><p>($classcode)</p></li>";
+       } 
+        
+        
        foreach ($ast as $ast) {
            $assistant_name = $ast['ast_name'];
-           $course_code = $ast['courseCode'];
-           echo "<li><p>$assistant_name ($course_code)</p></li>";
+//           $course_code = $ast['courseCode'];
+           echo "<li><p>$assistant_name</p></li>";
        }   
           ?>
     </ul>
