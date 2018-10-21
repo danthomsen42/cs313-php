df3765d23db03e1e2ae7fa012a78f3d750e24fdf
Daniel_Thomsen <tho13020@byui.edu>
2018-10-20 19:43:22 -0600

    mah first attempt at sending to mah db

--- db/prjOne.sql
+++ db/prjOne.sql
@@ -29,3 +29,21 @@ CREATE TABLE students
     , start_time TIMESTAMP
     , end_time TIMESTAMP
 );
+
+INSERT INTO classes(courseCode) VALUES ('CIT_111');
+INSERT INTO classes(courseCode) VALUES ('CIT_225');
+INSERT INTO classes(courseCode) VALUES ('CIT_325');
+INSERT INTO classes(courseCode) VALUES ('CIT_425');
+
+INSERT INTO assistants(ast_name) VALUES ('William');
+INSERT INTO assistants(ast_name) VALUES ('Brooke');
+INSERT INTO assistants(ast_name) VALUES ('Andrew');
+
+INSERT INTO assistant_classes(ast_name, courseCode) VALUES
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 2),
+(2, 3),
+(3, 1),
+(3, 2);
---
+++ web/cit_lab/displayMain.php
@@ -0,0 +1,33 @@
+<?php
+//conect to DB
+require('dbConnect.php');
+//$db = get_db();
+//query for all movies
+$stmt = $db->prepare('SELECT id, ast_name, courseCode FROM assistant_classes');
+$stmt-> execute();
+
+$ast = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// go through each movie in th eresult and display it
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    
+    
+    </head>
+<body>
+    <ul>
+    <?php
+       foreach ($ast as $ast) {
+           $assistant_name = $ast['ast_name'];
+           $course_code = $ast['courseCode'];
+           echo "<li><p>$assistant_name ($course_code)</p></li>";
+       }   
+          ?>
+    </ul>
+    
+    </body>
+
+
+</html>
