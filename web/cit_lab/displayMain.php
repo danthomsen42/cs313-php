cf7775c424e811b2123715fb0b34c921fad92d48
Daniel_Thomsen <tho13020@byui.edu>
2018-10-22 16:44:40 -0600

    fixed?

--- web/cit_lab/displayMain.php
+++ web/cit_lab/displayMain.php
@@ -3,9 +3,9 @@
 require('dbConnect.php');
 //$db = get_db();
 //query for all movies
-//$stmt = $db->prepare('SELECT id, ast_name FROM assistants');
+$stmt = $db->prepare('SELECT id, ast_name FROM assistants');
 $meh = $db->prepare('SELECT id, course_code FROM classes');
-//$stmt-> execute();
+$stmt-> execute();
 $meh-> execute();
 
 //$ast = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -27,15 +27,15 @@ $cls = $meh->fetchAll(PDO::FETCH_ASSOC);
            $id = $class['id'];
 //           $course_code = $ast['courseCode'];
            echo "<li><p>$id () $classcode</p></li>";
-           var_dump($class);
+//           var_dump($class);
        } 
         
         
-//       foreach ($ast as $ast) {
-//           $assistant_name = $ast['ast_name'];
-////           $course_code = $ast['courseCode'];
-//           echo "<li><p>$assistant_name</p></li>";
-//       }   
+       foreach ($ast as $assist) {
+           $assistant_name = $assist['ast_name'];
+//           $course_code = $ast['courseCode'];
+           echo "<li><p>$assistant_name</p></li>";
+       }   
           ?>
     </ul>
     
