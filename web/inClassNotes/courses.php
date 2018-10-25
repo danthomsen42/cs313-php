315e76d922363a663e021232ae93bfaf7e67e02c
Daniel_Thomsen <tho13020@byui.edu>
2018-10-25 16:12:48 -0600

    send

---
+++ web/inClassNotes/courses.php
@@ -0,0 +1,63 @@
+<?php
+
+require('dbConnect.php'); 
+
+$db = get_db();
+
+$query = 'SELECT id, code, name FROM course';
+
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Courses</title>
+
+
+    </head>
+
+    <body>
+        <h1>Courses</h1>
+
+
+        <ul>
+            
+            <?php 
+            foreach ($courses as $course){
+                $id = $course['id'];
+                $name = $course['name'];
+                $code = $course['code'];
+                
+                echo '<li><p>$code - $name</p></li>\n';
+                
+            }
+            
+            ?>
+            <li>
+                <p>CS 313 -Web II</p>
+            </li>
+            <li>
+                <p>CS 313 -Web II</p>
+            </li>
+            <li>
+                <p>CS 313 -Web II</p>
+            </li>
+            <li>
+                <p>CS 313 -Web II</p>
+            </li>
+
+
+
+        </ul>
+
+
+    </body>
+
+
+
+    </html>
