af7831fb9fcbfeacb459f1898a79055b24a993cf
Daniel_Thomsen <tho13020@byui.edu>
2018-10-26 19:03:49 -0600

    send

--- web/group-prj-wk5/scripturesResult.php
+++ web/group-prj-wk5/scripturesResult.php
@@ -1,8 +1,9 @@
 <?php 
  require 'resources.php';
 $fix = $_POST['topic'];
-$arr = unserialize(base64_decode($fix));
-foreach ($arr as $ar){
+//$arr = unserialize(base64_decode($fix));
+
+foreach ($fix as $ar){
 
 echo $ar;
 }
