3fbe7cf7c233021a8dcf8fafa0861daad22fbcf3
Daniel_Thomsen <tho13020@byui.edu>
2018-10-30 16:59:52 -0600

    week7 team

--- web/week7_Team/welcome.php
+++ web/week7_Team/welcome.php
@@ -6,11 +6,13 @@
 
     require 'resources.php';
     $db = get_db();
-    $pas = $_POST["password"];
-    $usr = $_POST["username"];    
+    $pas = $_POST['password'];
+    $usr = $_POST['username'];    
         $passwordHash = password_hash($pas, PASSWORD_DEFAULT);
         
-    $statement = $db->prepare("INSERT INTO loginUserTeamWeekSeven (username, password) VALUES (" .$usr.",". $passwordHash.");");
+    $statement = $db->prepare("INSERT INTO loginUserTeamWeekSeven (username, password) VALUES (usr, passwordHash);");
+    statement->bindValue(':usr', $usr, PDO::PARAM_STR);
+    statement->bindValue(':pas', $pas, PDO::PARAM_STR);
     $statement->execute();
     
     
