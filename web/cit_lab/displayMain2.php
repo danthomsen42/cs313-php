c9e625f466b1fa457879f6ce3d05f8a6737a5d5f
Daniel_Thomsen <tho13020@byui.edu>
2018-10-31 22:16:24 -0600

    I'm getting it...

--- web/cit_lab/displayMain2.php
+++ web/cit_lab/displayMain2.php
@@ -72,16 +72,13 @@ $StLst = $StudentList->fetchAll(PDO::FETCH_ASSOC);
               echo '</br>'; 
               echo '</br>'; 
              echo '</br>';
-            echo '</br>';
-            echo '</br>';
-            echo '</br>';
-            echo '</br>';
+
             echo '<p>Notes:</p></br>';
-            echo '</br><textarea cols=40 rows=3 placeholder="Notes"></textarea>';
+          
               echo '</br>';
             
        echo '</select>';  
-            
+              echo '</br><textarea cols=40 rows=3 placeholder="Notes"></textarea>';
     echo '</br>';
          
     echo '<h3>Not on the list? Sign up down below to be added to the list. </h3>';        
