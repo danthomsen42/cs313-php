477a386469f35bde16ed88b8dd844fb1353af8f0
Daniel_Thomsen <tho13020@byui.edu>
2018-10-31 22:19:38 -0600

    I'm getting it...

--- web/cit_lab/displayMain2.php
+++ web/cit_lab/displayMain2.php
@@ -83,6 +83,9 @@ $StLst = $StudentList->fetchAll(PDO::FETCH_ASSOC);
               echo '<textarea cols=40 rows=3 placeholder="Notes"></textarea>';
     echo '</br>';
          
+        echo '<input type="submit" value="Submit">';            
+            
+            
     echo '<h3>Not on the list? Sign up down below to be added to the list. </h3>';        
         
             
