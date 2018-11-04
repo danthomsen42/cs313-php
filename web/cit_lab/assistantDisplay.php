bf12b6cdaafc0fd04db93ae66fbb44b412b778cd
Daniel_Thomsen <tho13020@byui.edu>
2018-11-03 23:10:39 -0600

    queue

--- web/cit_lab/assistantDisplay.php
+++ web/cit_lab/assistantDisplay.php
@@ -6,9 +6,23 @@
     
       <form method="POST">
         
+          
+          
+          
         <?php  
           require('dbConnect.php');
           $db = get_db();
+          
+          //Display the next student in the queue that the assistant is assigned to
+          
+          //When the button is pressed, it UPDATES queue and sets the end_time to the current time stamp
+          
+          //figure out how to auto increment to next availbale individual according to courses assitant's can teach, if students are already being helped or not, and if not, being assigned to that next availble student with the course the assistant can teach
+          
+          
+          //When Assistant is assigned, update the Start_time part in the queue.
+          
+          //Don't Bother with a skip button at this time
           
           $Assistant_finish = $db->prepare('UPDATE queue SET end_time = now() WHERE queue.id = 3');
           
