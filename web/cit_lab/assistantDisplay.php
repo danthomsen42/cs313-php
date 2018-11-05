94101f45f49791b5f6cfb020b62a0c630656269f
Daniel_Thomsen <tho13020@byui.edu>
2018-11-05 12:59:06 -0700

    queue

--- web/cit_lab/assistantDisplay.php
+++ web/cit_lab/assistantDisplay.php
@@ -13,8 +13,35 @@
           require('dbConnect.php');
           $db = get_db();
           
-
+          $StudentByID = $db->prepare('SELECT students.student_first_name, students.student_last_name, queue.id, student_name, queue.course_code as queue_course_code, ast_name, enter_time, start_time, end_time, comments, classes.course_code as classes_course_code 
+FROM students
+JOIN queue ON queue.student_name = students.id
+JOIN classes ON queue.course_code = classes.id
+WHERE queue.student_name = students.id
+ORDER BY queue.id;')
+     $StudentByID-> execute();
+    $StuID = $StudentByID->fetchAll(PDO::FETCH_ASSOC);
           
+    echo '<select name="StudentName">';
+            foreach ($StuID as $Que) {
+            $endTime = $Que['end_time'];
+            
+//            $studentIndex = $Que['student_name'];
+            $studentFirstName = $Que['student_first_name'];
+                $studentLastName = $Que['student_last_name'];
+            $queueID = $Que['queue.id']
+//            $studentName = $Que['student_name'];
+            //$courseCode = $Que['classes_course_code'];
+            $startTime = $Que['start_time'];
+           // $assistantName = $Que['ast_name'];
+         //   $roger = $Que['comments'];
+            if ($endTime === NULL){      
+          echo '<option value='.$queueID.'>'.$studentFirstName.' '.$studentLastName. '</option>';
+                
+                
+            }
+            }
+               echo '</select>';   
           //Display the next student in the queue that the assistant is assigned to
           
           //When the button is pressed, it UPDATES queue and sets the end_time to the current time stamp
@@ -30,6 +57,12 @@
           
           
           $Assistant_finish->execute();
+          
+          echo '<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>';
           
           
           ?>
