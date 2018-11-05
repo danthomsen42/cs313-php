838438bbf5e162ebb961b73b7330d38dbb907c8a
Daniel_Thomsen <tho13020@byui.edu>
2018-11-05 13:20:24 -0700

    queue

--- web/cit_lab/assistantDisplay.php
+++ web/cit_lab/assistantDisplay.php
@@ -13,7 +13,7 @@
           require('dbConnect.php');
           $db = get_db();
           
-          $StudentByID = $db->prepare('SELECT students.student_first_name, students.student_last_name, queue.id, student_name, queue.course_code as queue_course_code, ast_name, enter_time, start_time, end_time, comments, classes.course_code as classes_course_code 
+$StudentByID = $db->prepare('SELECT students.student_first_name, students.student_last_name, queue.id, student_name, queue.course_code as queue_course_code, ast_name, enter_time, start_time, end_time, comments, classes.course_code as classes_course_code 
 FROM students
 JOIN queue ON queue.student_name = students.id
 JOIN classes ON queue.course_code = classes.id
@@ -21,20 +21,21 @@ WHERE queue.student_name = students.id
 ORDER BY queue.id;');
      $StudentByID-> execute();
     $StuID = $StudentByID->fetchAll(PDO::FETCH_ASSOC);
+
+          
+$Assistants = $db->prepare('SELECT id, ast_name FROM assistants');
+$Assistants->execute();
+$asstnt = $Assistants->fetchAll(PDO::FETCH_ASSOC);
+          
           
     echo '<select name="StudentName">';
             foreach ($StuID as $Que) {
             $endTime = $Que['end_time'];
-            
-//            $studentIndex = $Que['student_name'];
             $studentFirstName = $Que['student_first_name'];
                 $studentLastName = $Que['student_last_name'];
             $queueID = $Que['queue.id'];
-//            $studentName = $Que['student_name'];
             $courseCode = $Que['classes_course_code'];
             $startTime = $Que['start_time'];
-           // $assistantName = $Que['ast_name'];
-         //   $roger = $Que['comments'];
             if ($endTime === NULL){      
           echo '<option value='.$queueID.'>'.$studentFirstName.' '.$studentLastName.' ------- '. $courseCode.'</option>';
                 
@@ -42,6 +43,17 @@ ORDER BY queue.id;');
             }
             }
                echo '</select>';   
+          
+    echo '<select name="AssistantName">';
+          foreach (asstnt as ast){
+              $astID = $ast['id'];
+              $astName = $ast['ast_name'];
+          echo '<option value='.$astID.'>'.$astName.'</option>';
+          
+          }
+          echo '</select>';
+          
+          
           //Display the next student in the queue that the assistant is assigned to
           
           //When the button is pressed, it UPDATES queue and sets the end_time to the current time stamp
